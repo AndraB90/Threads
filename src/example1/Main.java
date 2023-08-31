dc5e3df5f38bb5a893d6a438056126d3c77d5583
Andra <andra.botean@gmail.com>
2023-08-31 19:11:11 +0200

    Resolved PR comments

--- src/example1/Main.java
+++ src/example1/Main.java
@@ -7,29 +7,27 @@ public class Main {
         System.out.println("Hello");
 
         // Print how many Threads are active now
-        System.out.println("Number of active Threads: "+Thread.activeCount());
+        System.out.println("Number of active Threads: " + Thread.activeCount());
         Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
-        for (var thread: threadSet) {
-            System.out.println(thread+" is daemon: "+thread.isDaemon());
+        for (var thread : threadSet) {
+            System.out.println(thread + " is daemon: " + thread.isDaemon());
         }
 
         // Print the name of active Thread
-        System.out.println("Name of active Thread: "+Thread.currentThread().getName());
+        System.out.println("Name of active Thread: " + Thread.currentThread().getName());
 
         // Change name of active Thread
         Thread.currentThread().setName("Mircea");
-        System.out.println("Name of active Thread: "+Thread.currentThread().getName());
+        System.out.println("Name of active Thread: " + Thread.currentThread().getName());
 
         // Show priority of main Thread
-        System.out.println("Priority of Thread "+Thread.currentThread().getName()+" is: "+Thread.currentThread().getPriority());
+        System.out.println("Priority of Thread " + Thread.currentThread().getName() + " is: " + Thread.currentThread().getPriority());
 
         // Change priority of main Thread
         Thread.currentThread().setPriority(10);
-        System.out.println("Priority of Thread "+Thread.currentThread().getName()+" is: "+Thread.currentThread().getPriority());
+        System.out.println("Priority of Thread " + Thread.currentThread().getName() + " is: " + Thread.currentThread().getPriority());
 
         // Show if current Thread is active
-        System.out.println("Thread "+Thread.currentThread().getName()+" is activ: "+Thread.currentThread().isAlive());
-
-
+        System.out.println("Thread " + Thread.currentThread().getName() + " is activ: " + Thread.currentThread().isAlive());
     }
 }
